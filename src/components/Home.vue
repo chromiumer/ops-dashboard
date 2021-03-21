647c5491f72001297153d7a6e005c8c719739652
007 <waajueji@gmail.com>
2021-03-21 22:47:02 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,48 @@
 <template>
-  <div>
-    home 组件
+  <el-container class="home-container">
+  <!-- 头部区域 -->
+  <el-header>
+      <div>
+      <img src="../assets/chouju.jpg" width="55px" height="50px" alt="">
+      <span>OPS后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+  </el-header>
+  <!-- 页面主体区域 -->
+  <el-container>
+    <!-- 侧边栏 -->
+    <el-aside width="200px">
 
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+    <!-- 侧边栏菜单区域 -->
+    <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+      <!-- 一级菜单 -->
+      <el-submenu index="1">
+          <!-- 一级菜单模板区域 -->
+        <template slot="title">
+          <!-- 图标 -->
+          <i class="el-icon-location"></i>
+          <!-- 文本导航名称 -->
+          <span>导航一</span>
+        </template>
+
+        <!-- 二级菜单 -->
+        <el-menu-item index="1-4-1">
+            <template slot="title">
+            <!-- 图标 -->
+            <i class="el-icon-location"></i>
+            <!-- 文本导航名称 -->
+            <span>导航一</span>
+            </template>
+        </el-menu-item>
+
+      </el-submenu>
+    </el-menu>
+
+    </el-aside>
+    <!-- 右侧内容主体 -->
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -18,5 +57,34 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+
+.home-container {
+    height: 100%;
+}
+
+ .el-header {
+     background-color: #373d41;
+     display: flex;
+     justify-content: space-between;
+     padding-left: 0;
+     align-items: center;
+     color: #fff;
+     font-size: 20px;
+      > div {
+         display: flex;
+         align-items: center;
+         span {
+             margin-left: 15px;
+         }
+     }
+ }
+
+ .el-aside {
+     background-color: #333744;
+ }
+
+ .el-main {
+     background-color: #eaedf1;
+ }
 </style>
